a748cb5b948691d611b230db47eaa420681054f4
Sandaru Gunathilake <maduhansadilshan@gmail.com>
2025-04-12 08:52:01 +0530

    add useEffect to dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,30 +1,44 @@
-import React, { useEffect } from 'react'
-import Header from '../components/Header';
-import SideBar from '../components/SideBar';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-  
-    if (!token) {
-      console.error("No token found. Redirecting to login...");
-      setTimeout(() => navigate("/login", { replace: true }), 0);
+    async function fetchData() {
+      if (!token) {
+        console.error("No token found. Redirecting to login...");
+        setTimeout(() => navigate("/login", { replace: true }), 0);
+        return;
+      }
+
+      try {
+        const response = await axios.get(`${apiBaseUrl}/villagers/north`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
     }
-  }, [navigate]);  
+
+    fetchData();
+  }, [token]);
 
   return (
-    <div className='d-flex bg-gradient'>
+    <div className="d-flex bg-gradient">
       <SideBar />
-      <div className='flex-grow-1'>
+      <div className="flex-grow-1">
         <Header />
         {/* content  */}
       </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
