2e436efb0970afdc24854063747f77d13b9b817a
Sandaru Gunathilake <maduhansadilshan@gmail.com>
2025-04-16 08:08:17 +0530

    create dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,57 +6,169 @@ import { useNavigate } from "react-router-dom";
 function Dashboard() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function fetchData() {
-      if (!token) {
-        console.error("No token found. Redirecting to login...");
-        setTimeout(() => navigate("/login", { replace: true }), 0);
-        return;
-      }
+  // useEffect(() => {
+  //   async function fetchData() {
+  // if (!token) {
+  //   console.error("No token found. Redirecting to login...");
+  //   setTimeout(() => navigate("/login", { replace: true }), 0);
+  //   return;
+  // }
 
-      try {
-        const response1 = await axios.get(`${apiBaseUrl}/villagers/north`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        const response2 = await axios.get(`${apiBaseUrl}/villagers/east`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        const response3 = await axios.get(`${apiBaseUrl}/villagers/south`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        const response4 = await axios.get(`${apiBaseUrl}/villagers/west`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
-          },
-        });
-        console.log(response1);
-        console.log(response2);
-        console.log(response3);
-        console.log(response4);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
-    }
+  //   try {
+  //     const response1 = await axios.get(`${apiBaseUrl}/villagers/north`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const response2 = await axios.get(`${apiBaseUrl}/villagers/east`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const response3 = await axios.get(`${apiBaseUrl}/villagers/south`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const response4 = await axios.get(`${apiBaseUrl}/villagers/west`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     console.log(response1);
+  //     console.log(response2);
+  //     console.log(response3);
+  //     console.log(response4);
+  //   } catch (error) {
+  //     console.error("Error fetching users:", error);
+  //   }
+  // }
 
-    fetchData();
-  }, [token]);
+  // fetchData();
+  // }, [token]);
 
   return (
     <div className="d-flex bg-gradient">
       <SideBar />
       <div className="flex-grow-1">
         <Header />
-        {/* content  */}
+        <div className="app-content">
+          <div className="container-fluid">
+            <div className="row d-flex justify-content-center align-content-center">
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-warning">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>Users Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-success">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>Villagers Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-danger">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>North villagers Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-secondary">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>East villagers Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-info">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>South villagers Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-6">
+                <div className="small-box text-bg-primary">
+                  <div className="inner">
+                    <h3>5</h3>
+                    <p>West villagers Count</p>
+                  </div>
+                  <svg
+                    className="small-box-icon"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
